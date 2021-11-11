c153e8a1c3e514c669454c67b75e89b60bfaf884
Zi Nean Teoh <zinean00@gmail.com>
2021-11-10 18:49:38 -0600

    created transaction supply table

---
+++ models/transaction-supply-table.js
@@ -0,0 +1,32 @@
+import Sequelize from "sequelize";
+
+import { connectDB as connectSQLDB } from "../db/index.js";
+
+var sequelize;
+
+export class SQTransactionSupply extends Sequelize.Model {}
+
+// Connects to database and creates the table.
+export async function connectDB() {
+	if (sequelize) return;
+	sequelize = await connectSQLDB();
+	SQTransactionSupply.init(
+		{
+			transactionId: {
+				type: Sequelize.DataTypes.INTEGER,
+				primaryKey: true,
+				unique: true,
+			},
+
+			item1: Sequelize.DataTypes.INTEGER,
+			item2: Sequelize.DataTypes.INTEGER,
+			item3: Sequelize.DataTypes.INTEGER,
+			phone: Sequelize.DataTypes.INTEGER,
+		},
+		{
+			sequelize,
+			modelName: "SQTeacher",
+		}
+	);
+	await SQTransactionSupply.sync();
+}
