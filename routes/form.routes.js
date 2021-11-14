f3ddefcb5958749c4c3712d3246e331b65cd47ec
Zi Nean Teoh <zinean00@gmail.com>
2021-11-14 13:41:51 -0600

    changed addSupply from get to post request

--- routes/form.routes.js
+++ routes/form.routes.js
@@ -3,15 +3,15 @@ import formController from "../controllers/form.controller.js";
 
 const router = express.Router();
 
-router.route("/addSupply").get(formController.addSupply); // add Supply - TESTING ONLY! To be deleted.
+router.route("/addSupply").post(formController.addSupply); // add Supply to shopping form
 
 router.route("/getShopForm").get(formController.fetchShopForm); // fetch Supply Form
 
 router.route("/:teacherID").get(formController.getTeacher); // get teacher information for form
 
-router.route("/create").post(formController.addTeacher); // add teacher to database
-
 router.param("teacherID", formController.teacherByID); // get teacher by id
+
+router.route("/create").post(formController.addTeacher); // add teacher to database
 
 router.route("/transaction/submit").post(formController.submitTransaction);
 
