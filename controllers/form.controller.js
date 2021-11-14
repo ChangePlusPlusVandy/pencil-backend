cd976203df6c2dc682b94a4b3b4c9d47f2291c8e
Zi Nean Teoh <zinean00@gmail.com>
2021-11-14 14:31:17 -0600

    minor changes to form controller

--- controllers/form.controller.js
+++ controllers/form.controller.js
@@ -3,15 +3,10 @@ import {
 	SQTeacher,
 } from "../models/teacher-table.js";
 import {
-<<<<<<< HEAD
-	connectDB as connectSupplyFormDB,
-	SQShoppingForm,
-} from "../models/shopping-form-table.js";
-=======
+
     connectDB as connectSupplyFormDB,
     SQShoppingForm
 } from '../models/shopping-form-table.js';
->>>>>>> arthur-backend
 import {
 	connectDB as connectTransactionDB,
 	SQTransaction,
