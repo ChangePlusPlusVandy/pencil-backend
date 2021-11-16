1009e6545b32bd9f82c2c4de905c08191e57135d
Intiser Rajoan Parash <irparash@gmail.com>
2021-11-15 20:40:03 -0600

    removed deprecated bodyparser

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-import sampleRoutes from './routes/sample.routes.js'
-import formRoutes from './routes/form.routes.js'
-import bodyParser from 'body-parser'
+import sampleRoutes from './routes/sample.routes';
+import formRoutes from './routes/form.routes';
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 
-app.use('/sample', sampleRoutes)
+app.use('/sample', sampleRoutes);
 
-app.use('/api/form', formRoutes)
+app.use('/api/form', formRoutes);
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-const server = app.listen(port, (err) => {
-    if(err) {
-        console.log(err)
-    }
-    console.log(`Server is listening on port ${port}`)
-})
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Server is listening on port ${port}`);
+});
 
-export default app
+export default app;
