cbf547a242fb46bac207395e3a97b4dea009b1b3
RaffaVoo <aidanva34@gmail.com>
2021-12-05 14:07:38 -0600

    Create transactionByID
    Created the function transactionByID, which retrieves a transaction from the SQTempTransaction table given its id. Added this function to the helpers/ directory.

---
+++ helpers/form.helper.js
@@ -0,0 +1,27 @@
+import {
+  connectDB,
+  SQTempTransaction
+} from '../models/temp-transaction-table.js';
+
+/**
+ * Retrieves a row from the tempTransactionDB, given a transaction ID.
+ * @param {Number} id - The ID of the transaction to be retrieved
+ * @return {Object} - The transaction corresponding to the specified ID or undefined (its type
+ *                    is technically object, but it has the functions of a sequelize instance)
+ */
+const transactionByID = async (id) => {
+  try {
+    await connectDB();
+    const transaction = await SQTempTransaction.findOne({
+      where: {
+        transactionId: id,
+      }
+    });
+
+    return transaction;
+  } catch {
+    console.log('Could not retrieve transaction from temp table');
+  }
+};
+
+export default transactionByID;
