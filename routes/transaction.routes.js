62aefab473d4ab7eb6f29ad7bb79adee4cd66941
Kylie Burgess <burgess21k@gmail.com>
2022-02-04 17:02:24 -0500

    Prettier changes

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,27 +1,27 @@
-import express from "express";
-import transactionController from "../controllers/transaction.controller.js";
+import express from 'express';
+import transactionController from '../controllers/transaction.controller.js';
 
 const router = express.Router();
 
 router
-  .route("/transaction/submit")
+  .route('/transaction/submit')
   .post(transactionController.submitTransaction); // submit transaction to temp table
 
 router
-  .route("/transaction/approve")
+  .route('/transaction/approve')
   .post(transactionController.approveTransaction); // approve transaction from temp table to transaction table
 
 router
-  .route("/transaction/transactions")
+  .route('/transaction/transactions')
   .get(transactionController.getAllTransactions); // get all transactions from temp table
 
-router.route("/transaction/deny").post(transactionController.denyTransaction);
+router.route('/transaction/deny').post(transactionController.denyTransaction);
 
 router
-  .route("/transaction/:transactionID")
+  .route('/transaction/:transactionID')
   .get(transactionController.getTransaction); // get one transaction
 
-router.param("transactionID", transactionController.transactionByID); // param for transactionID
+router.param('transactionID', transactionController.transactionByID); // param for transactionID
 // eslint-disable-next-line prettier/prettier
 router
   .route('/transaction/deny')
