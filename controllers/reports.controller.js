d62fe365845c88b223c5be53551a8c6e8deb43b6
Arthur Sung <arthursung98@gmail.com>
2022-03-28 19:31:08 -0500

    Report : middleware function done.

--- controllers/reports.controller.js
+++ controllers/reports.controller.js
@@ -11,9 +11,21 @@ const { teacherByID } = require('./teacher.controller.js');
 // const ExcelJS = require('exceljs/dist/es5');
 const teacher = require('../models/teacher');
 
+// eslint-disable-next-line consistent-return
 const getTransaction = async (req, res, next) => {
   try {
-    let transactions = await Transaction.findAll({
+    const transactionWhereStatement = {};
+    if (req.query.startDate && req.query.endDate) {
+      transactionWhereStatement.createdAt = {
+        [Op.between]: [req.query.startDate, req.query.endDate],
+      };
+    }
+    const schoolWhereStatement = {};
+    if (req.query.school) {
+      schoolWhereStatement.uuid = req.query.school;
+    }
+
+    const transactions = await Transaction.findAll({
       attributes: ['createdAt'],
       where: transactionWhereStatement,
       include: [
@@ -33,6 +45,9 @@ const getTransaction = async (req, res, next) => {
         },
       ],
     });
+
+    req.transactions = transactions;
+    next();
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: err });
@@ -42,57 +57,33 @@ const getTransaction = async (req, res, next) => {
 // Report 1 : - Date shopped,Teacher name,Teacher email,Teacher school,Value of products.
 // Elements of list are individual shopping trips by teachers.
 const report1 = async (req, res) => {
-  // Construct where statement for transaction query according to passed parameters
-  const transactionWhereStatement = {};
-  if (req.query.startDate && req.query.endDate) {
-    transactionWhereStatement.createdAt = {
-      [Op.between]: [req.query.startDate, req.query.endDate],
-    };
-  }
+  // Construct where statement for transaction query according to passed parameters.
+  // NOTE : Aidan & Arthur look at these query statements and see if its appropriate.
+  console.log('THIS IS THE REQ BODY: ', req.body);
+  getTransaction(req, res);
+  // eslint-disable-next-line prefer-destructuring
+  const transactions = req.transactions;
 
-  const schoolWhereStatement = {};
-  if (req.query.school) {
-    schoolWhereStatement.uuid = req.query.school;
-  }
+  const pricedTransactions = transactions.map((transaction) => {
+    let cumulativeItemPrice = 0;
 
-  try {
-    let transactions = await Transaction.findAll({
-      attributes: ['createdAt'],
-      where: transactionWhereStatement,
-      include: [
-        {
-          model: TransactionItem,
-          include: {
-            model: Item,
-          },
-        },
-        {
-          model: Teacher,
-          attributes: ['firstName', 'lastName', 'email'],
-        },
-        {
-          model: School,
-          where: schoolWhereStatement,
-        },
-      ],
+    transaction.TransactionItems.forEach((transactionItem) => {
+      cumulativeItemPrice +=
+        transactionItem.dataValues.Item.dataValues.itemPrice;
     });
 
-    for (transIndex in transactions) {
-      let cumulativeItemPrice = 0;
-      for (let item of transactions[transIndex].TransactionItems) {
-        cumulativeItemPrice +=
-          item.dataValues.Item.dataValues.itemPrice *
-          item.dataValues.amountTaken;
-      }
+    // eslint-disable-next-line
+    transaction.totalItemPrice = cumulativeItemPrice;
 
-      transactions[transIndex].dataValues.totalItemPrice = cumulativeItemPrice;
-    }
+    console.log('the total price is ', transaction.totalItemPrice);
+    console.log('For transaction: ', transaction.TransactionItems);
+    console.log('\n\n\n');
+    return transaction;
+  });
 
-    return res.status(200).json(transactions);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: err });
-  }
+  console.log('THESE ARE THE TRANSACTIONS: ', transactions);
+
+  return res.status(200).json(pricedTransactions);
 };
 
 module.exports = {
