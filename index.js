34d51c0eb56e0c947ea5a6858aa499edf13c3e6c
Kevin Jin <kevjin2002@gmail.com>
2022-04-02 23:03:09 -0500

    Fixed routes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/api/:location/form', formRoutes);
 app.use('/api/teacher', teacherRoutes);
 app.use('/api/:location/transaction', transactionRoutes);
 app.use('/api/location', locationRoutes);
-app.use('/api/:location/schedule', schedulerRoutes);
+app.use('/api/schedule', schedulerRoutes);
 app.use('/api/masterInventory', masterInventoryRoutes);
 
 const port = process.env.PORT || 8080;
--- routes/scheduler.routes.js
+++ routes/scheduler.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const schedulerController = require('../controllers/scheduler.controller.js');
+const locationController = require('../controllers/location.controller.js');
 
 const router = express.Router();
 
-router.route('/getSchedule').get(schedulerController.getSchedule);
+router.route('/:location/getSchedule').get(schedulerController.getSchedule);
 router.route('/addAppointment').post(schedulerController.addAppointment);
+router.param('location', locationController.locationByID);
 
 module.exports = router;
