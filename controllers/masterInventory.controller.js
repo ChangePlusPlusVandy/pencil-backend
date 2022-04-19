69d7b549a96fc6348ee30730ed44e30fd7e7a46b
Kevin Jin <kevjin2002@gmail.com>
2022-04-19 15:54:02 -0500

    Fixed archive

--- controllers/masterInventory.controller.js
+++ controllers/masterInventory.controller.js
@@ -48,10 +48,7 @@ const getAllItems = async (req, res, next) => {
   try {
     const itemList = await Item.findAll({
       order: [['itemName', 'ASC']],
-      attributes: ['uuid', 'itemName', 'itemPrice'],
-      where: {
-        archived: false,
-      },
+      attributes: ['uuid', 'itemName', 'itemPrice', 'archived'],
     });
 
     return res.status(200).json(itemList);
@@ -63,12 +60,9 @@ const getAllItems = async (req, res, next) => {
 
 const updateMasterInventory = async (req, res, next) => {
   // FIXME: Consult about master inventory
-  const responseItem = [];
   try {
-    let allItems = await Item.findAll({});
-    console.log(allItems);
     req.body.forEach(async (item) => {
-      const [findSchedule, created] = await Item.findOrCreate({
+      const [findItem, created] = await Item.findOrCreate({
         where: {
           uuid: item.uuid,
         },
@@ -78,19 +72,16 @@ const updateMasterInventory = async (req, res, next) => {
         },
       });
       if (!created) {
-        allItems = allItems.filter((i) => i.Item.dataValues.uuid !== item.uuid);
-        await findSchedule.update({
-          archived: false,
+        await findItem.update({
+          archived: item.archived,
         });
       }
-      responseItem.push(findSchedule);
     });
-    allItems.forEach(async (item) => {
-      await item.update({
-        archived: true,
-      });
+    const itemList = await Item.findAll({
+      order: [['itemName', 'ASC']],
+      attributes: ['uuid', 'itemName', 'itemPrice', 'archived'],
     });
-    return res.status(200).json(responseItem);
+    return res.status(200).json(itemList);
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: 'Internal server error' });
--- controllers/scheduler.controller.js
+++ controllers/scheduler.controller.js
@@ -115,8 +115,58 @@ const cancelAppointment = async (req, res) => {
   }
 };
 
+const fakeAppointment = async (req, res) => {
+  try {
+    const location = await Location.findOne({
+      name: req.body.location,
+    });
+    const [findSchedule] = await Schedule.findOrCreate({
+      where: {
+        start_date: req.body.start_time,
+        end_date: req.body.end_time,
+        _locationId: location._id,
+      },
+    });
+
+    const [findSchool] = await School.findOrCreate({
+      where: {
+        name: req.body.school, // FIX BASED ON ACTUAL FORM
+      },
+      defaults: {
+        verified: false,
+      },
+    });
+
+    const [findTeacher] = await Teacher.findOrCreate({
+      where: {
+        email: req.body.teacher.email,
+      },
+      defaults: {
+        name: req.body.teacher.name,
+        phone: req.body.teacher.phone, // FIX BASED ON ACTUAL FORM
+        _schoolId: findSchool._id,
+      },
+    });
+    findTeacher.update({
+      pencilId: findTeacher._id,
+    });
+    const newScheduleItem = await ScheduleItem.create({
+      _scheduleId: findSchedule._id,
+      _teacherId: findTeacher._id,
+    });
+
+    return res.status(200).json({
+      message: 'Successfully added fake appointment',
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ err: 'Error adding appointment' });
+  }
+};
+
 module.exports = {
   addAppointment,
   cancelAppointment,
   getSchedule,
+  fakeAppointment,
 };
