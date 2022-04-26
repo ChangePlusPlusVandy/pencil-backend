a0329de9df8a46abf34c2f41f5e19f1f1972cef6
Kevin Jin <kevjin2002@gmail.com>
2022-04-25 21:25:04 -0500

    Fixed uuid issues

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -61,11 +61,12 @@ const submitTransaction = async (req, res) => {
  */
 const approveTransaction = async (req, res) => {
   try {
+    console.log(req.params.transuuid, 'bruh');
     const finalTransaction = await Transaction.findOne({
       where: { uuid: req.params.transuuid },
     });
-    finalTransaction.status = 1;
-    await finalTransaction.save();
+    console.log('finalTransaction', finalTransaction);
+    await finalTransaction.update({ status: 1 });
     if (req.query.newSchool) {
       const newSchool = await School.findOne({
         where: { name: req.body.schoolName },
